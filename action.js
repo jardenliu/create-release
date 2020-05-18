fe4d65b94ea1d25fda5ecfe70223516777d81ee9
流君酱 <15915839707@163.com>
2020-05-18 11:27:22 +0800

    Update action.js

--- action.js
+++ action.js
@@ -1,105 +1,113 @@
-require("child_process").execSync("npm install @actions/core @actions/github", {
-  cwd: __dirname
-});
-const fs = require("fs");
-const core = require("@actions/core");
-const github = require("@actions/github");
-const api = new github.GitHub(core.getInput("token"));
+require('child_process').execSync('npm install @actions/core @actions/github', {
+  cwd: __dirname,
+})
+const fs = require('fs')
+const core = require('@actions/core')
+const github = require('@actions/github')
+const api = new github.GitHub(core.getInput('token'))
 
-const main = async () => {
-  const name = core.getInput("name");
-  const code = core.getInput("code");
-  const body = core.getInput("body");
-  const prerelease = core.getInput("prerelease") == "true";
-  const recreate = core.getInput("recreate") == "true";
-  const assets = core
-    .getInput("assets")
-    .split(" ")
-    .map(asset => asset.split(":"));
-  console.log(assets)
-  console.log(core.getInput("assets"))
-  console.log(github.context.sha)
-  if (recreate) {
-    await deleteReleaseIfExists(code);
-  }
-
-  const release = await api.repos.createRelease({
-    ...github.context.repo,
-    tag_name: code,
-    target_commitish: github.context.sha,
-    name,
-    body,
-    draft: false,
-    prerelease: prerelease
-  });
-
-  for (const [source, target, type] of assets) {
-    const data = fs.readFileSync(source);
-    api.repos.uploadReleaseAsset({
-      url: release.data.upload_url,
-      headers: {
-        ["content-type"]: type,
-        ["content-length"]: data.length
-      },
-      name: target,
-      file: data
-    });
-  }
-};
-
-async function deleteReleaseIfExists(code) {
-  let release;
+const getReleaseByTag = async (tag) => {
+  let release
   try {
     release = await api.repos.getReleaseByTag({
       ...github.context.repo,
-      tag: code
-    });
+      tag,
+    })
   } catch (err) {
-    console.log(err);
-    console.log("Release not found.. moving to creation");
+    console.log(err)
+    console.log('Release not found.. moving to creation')
   }
-  console.log(release)
-  
+  return release && release.data
+}
+
+const main = async () => {
+  const name = core.getInput('name')
+  const code = core.getInput('code')
+  const body = core.getInput('body')
+  const hash = core.getInput('hash')
+  const prerelease = core.getInput('prerelease') == 'true'
+  const recreate = core.getInput('recreate') == 'true'
+  const assets = core
+    .getInput('assets')
+    .split(' ')
+    .map((asset) => asset.split(':'))
+
+  let release = await getReleaseByTag(code)
+
+  const isSameCommit = release && release.target_commitish === github.context.sha
+
+  if (recreate && !isSameCommit) {
+    await deleteReleaseIfExists(code, release)
+  }
+
+  if (!release || (recreate && !isSameCommit) ) {
+    const createRelease = await api.repos.createRelease({
+      ...github.context.repo,
+      tag_name: code,
+      target_commitish: github.context.sha,
+      name,
+      body,
+      draft: false,
+      prerelease: prerelease,
+    })
+    release = createRelease.data
+  }
+
+  for (const [source, target, type] of assets) {
+    const data = fs.readFileSync(source)
+    api.repos.uploadReleaseAsset({
+      url: release.upload_url,
+      headers: {
+        ['content-type']: type,
+        ['content-length']: data.length,
+      },
+      name: target,
+      file: data,
+    })
+  }
+}
+
+async function deleteReleaseIfExists(code, release) {
   if (!release) {
-    return;
+    return
   }
   const deleteRelease = async () =>
     api.repos.deleteRelease({
       ...github.context.repo,
-      release_id: release.data.id
-    });
+      release_id: release.id,
+    })
 
   const deleteTagRef = async () =>
     api.git.deleteRef({
       ...github.context.repo,
-      ref: `tags/${code}`
-    });
+      ref: `tags/${code}`,
+    })
 
-  await retryOnFail(deleteRelease, 3);
-  await retryOnFail(deleteTagRef, 3);
+  await retryOnFail(deleteRelease, 3)
+  await retryOnFail(deleteTagRef, 3)
 }
 
 async function retryOnFail(asyncFunction, maxTries = 3) {
   if (maxTries < 1) {
-    throw `Retried ${maxTries}.. failed always. aborting`;
+    throw `Retried ${maxTries}.. failed always. aborting`
   }
   try {
-    await delay(1000);
-    return await asyncFunction();
+    await delay(1000)
+    return await asyncFunction()
   } catch (err) {
-    console.log(err);
-    console.log(`Retrying now...`);
-    retryOnFail(asyncFunction, maxTries - 1);
+    console.log(err)
+    console.log(`Retrying now...`)
+    retryOnFail(asyncFunction, maxTries - 1)
   }
 }
 
 async function delay(ms) {
-  await new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
+  await new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
 }
 
-main().catch(error => {
-  console.error(error);
-  core.setFailed(error.message);
-});
+main().catch((error) => {
+  console.error(error)
+  core.setFailed(error.message)
+})
